4bc035e68efe36620b1064450cdd69f869d62bf6
pratap-5 <142750642+pratap-5@users.noreply.github.com>
2024-05-23 13:18:18 +0530

    socket added

--- frontends/src/context/SocketContext.jsx
+++ frontends/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://chat-app-1-oa2k.onrender.com/", {
         query: {
           userId: authUser._id,
         },
